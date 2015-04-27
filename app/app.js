bdff19748f6f205c737ec3780b9ca0dd29aabed5
romainseb <sebastien.romain@gmail.com>
2015-04-28 01:46:14 +0200

    Add again ng-animate

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 // Declare app level module which depends on filters, and services
 angular.module('website', [
     //Angular modules
-    'ngRoute', 'ngTouch', 'duScroll', 'ui.bootstrap.collapse'
+    'ngRoute', 'ngTouch', 'duScroll', 'ui.bootstrap.collapse', "ngAnimate"
 ]);
+
+angular.module('website').config(function ($animateProvider) {
+    $animateProvider.classNameFilter(/angular-animate/);
+});

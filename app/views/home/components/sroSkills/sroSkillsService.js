e66e76d63fe4d49c6cdc6b263f8b26ee5c505ed1
romainseb <sebastien.romain@gmail.com>
2015-04-29 22:49:58 +0200

    data is set in service instead of controller

--- app/views/home/components/sroSkills/sroSkillsService.js
+++ app/views/home/components/sroSkills/sroSkillsService.js
@@ -1,4 +1,134 @@
 angular.module('website').service('SroSkillsService',
     function () {
+
+        this.skillCategories = [
+            {
+                categoryName: "Front End",
+                color: "#2980b9",
+                technologies: [
+                    {
+                        name: "AngularJS",
+                        percent: "95"
+                    },
+                    {
+                        name: "Javascript",
+                        percent: "95"
+                    },
+                    {
+                        name: "jQuery",
+                        percent: "90"
+                    },
+                    {
+                        name: "ReactJS",
+                        percent: "50"
+                    },
+                    {
+                        name: "HTML5",
+                        percent: "85"
+                    },
+                    {
+                        name: "CSS3",
+                        percent: "85"
+                    },
+                    {
+                        name: "Apache Flex",
+                        percent: "70"
+                    }
+                ]
+            },
+            {
+                categoryName: "Back End",
+                color: "#34495e",
+                technologies: [
+                    {
+                        name: "Java",
+                        percent: "85"
+                    },
+                    {
+                        name: "PHP",
+                        percent: "80"
+                    },
+                    {
+                        name: "NodeJS",
+                        percent: "80"
+                    }
+                ]
+            },
+            {
+                categoryName: "Frameworks & outils",
+                color: "#e67e22",
+                technologies: [
+                    {
+                        name: "Spring",
+                        percent: "60"
+                    },
+                    {
+                        name: "Hibernate",
+                        percent: "60"
+                    },
+                    {
+                        name: "Express",
+                        percent: "60"
+                    },
+                    {
+                        name: "Symfony",
+                        percent: "50"
+                    },
+                    {
+                        name: "eZ Publish",
+                        percent: "80"
+                    },
+                    {
+                        name: "Sails.js",
+                        percent: "50"
+                    }
+                ]
+            },
+            {
+                categoryName: "Bases de données",
+                color: "#16a085",
+                technologies: [
+                    {
+                        name: "MongoDB",
+                        percent: "70"
+                    },
+                    {
+                        name: "MySQL / MariaDB",
+                        percent: "80"
+                    },
+                    {
+                        name: "Oracle",
+                        percent: "40"
+                    }
+                ]
+            },
+            {
+                categoryName: "Outils",
+                color: "#c0392b",
+                technologies: [
+                    {
+                        name: "NPM",
+                        percent: "70"
+                    },
+                    {
+                        name: "Grunt",
+                        percent: "90"
+                    },
+                    {
+                        name: "Gulp",
+                        percent: "70"
+                    },
+                    {
+                        name: "Bower",
+                        percent: "80"
+                    }
+                ]
+            }
+        ];
+
+        this.getSkills = function getSkills() {
+            return this.skillCategories;
+        };
+
     }
 );
--- app/views/home/components/sroStudies/sroStudiesService.js
+++ app/views/home/components/sroStudies/sroStudiesService.js
@@ -1,4 +1,50 @@
 angular.module('website').service('SroStudiesService',
-	function (){
-	}
+    function () {
+
+        this.studies = [
+            {
+                name: "Certification eZ Publish 4.3",
+                date: "21 Décembre 2010",
+                type: "certification",
+                link: "http://auth.ez.no/certification/verify/383079"
+            },
+            {
+                name: "Master IFPRU",
+                subName: "Informatique à finalités professionnalisantes et recherche unifiée",
+                location: "Université de Montpellier 2",
+                date: "2008 - 2010",
+                type: "study",
+                link: "http://www.deptinfo.univ-montp2.fr/"
+            },
+            {
+                name: "Licence professionnelle ACPI",
+                subName: "Assistant Chef de Projet Informatique",
+                location: "Université de Montpellier 2",
+                date: "2007 - 2008",
+                type: "study",
+                link: "http://iutdepinfo.iutmontp.univ-montp2.fr/index.php/formations/licences-pro/acpi-genie-logiciel-blog"
+            },
+            {
+                name: "DUT GL",
+                subName: "Genie Logiciel",
+                location: "IUT Vannes",
+                date: "2004 - 2007",
+                type: "study",
+                link: "http://www.iutvannes.fr/dut-informatique-morbihan-bretagne/"
+            },
+            {
+                name: "Bac Scientifique",
+                location: "Lycée Dame du Vœu - Hennebont",
+                date: "2004",
+                type: "study",
+                link: "http://www.lyceeduvoeu.fr/"
+            }
+        ];
+
+
+        this.getStudies = function () {
+            return this.studies;
+        };
+
+    }
 );
